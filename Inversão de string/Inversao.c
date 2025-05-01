de6f0ca1dfb1a13846764215f4aca8d616f63866
laviniagonzales <laviniagondelima@gmail.com>
2025-04-30 21:43:20 -0300

    Update Inversao.c

--- Inversão de string/Inversao.c
+++ Inversão de string/Inversao.c
@@ -5,19 +5,19 @@ void inverterString(char *str, int inicio, int fim) {
     if (inicio >= fim)
         return;
 
-    // Troca os caracteres nas posições 'inicio' e 'fim'
+   
     char temp = str[inicio];
     str[inicio] = str[fim];
     str[fim] = temp;
 
-    // Chamada recursiva para o meio da string
+   
     inverterString(str, inicio + 1, fim - 1);
 }
 
 int main() {
     char texto[100];
     printf("Digite uma string: ");
-    scanf("%s", texto);  // Lê uma string sem espaços
+    scanf("%s", texto);  
 
     int tamanho = strlen(texto);
     inverterString(texto, 0, tamanho - 1);
