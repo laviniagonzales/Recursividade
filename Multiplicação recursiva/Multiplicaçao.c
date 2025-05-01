57bbfdf0fc3fb6f32312298e30b2ce38e1b39d83
laviniagonzales <laviniagondelima@gmail.com>
2025-04-30 21:17:22 -0300

    Update Multiplicaçao.c

---
+++ Multiplicação recursiva/Multiplicaçao.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int multiplicar(int a, int b) {
+    if (b == 0)  // Caso base: qualquer número multiplicado por 0 é 0
+        return 0;
+    return a + multiplicar(a, b - 1);  // Soma recursiva
+}
+
+int main() {
+    int num1, num2;
+    printf("Digite dois números inteiros não negativos: ");
+    scanf("%d %d", &num1, &num2);
+
+    printf("%d * %d = %d\n", num1, num2, multiplicar(num1, num2));
+
+    return 0;
+}
