d3bd7d7808237d9966496866b93fb2c61947bd4a
laviniagonzales <laviniagondelima@gmail.com>
2025-04-30 21:45:35 -0300

    Update Palindromo.c

--- Verificação de palíndromo/Palindromo.c
+++ Verificação de palíndromo/Palindromo.c
@@ -3,16 +3,16 @@
 
 int ehPalindromo(char *str, int inicio, int fim) {
     if (inicio >= fim)
-        return 1; // É palíndromo
+        return 1; 
     if (str[inicio] != str[fim])
-        return 0; // Não é palíndromo
+        return 0; 
     return ehPalindromo(str, inicio + 1, fim - 1);
 }
 
 int main() {
     char texto[100];
     printf("Digite uma string: ");
-    scanf("%s", texto);  // Lê uma palavra sem espaços
+    scanf("%s", texto);  
 
     int tamanho = strlen(texto);
     if (ehPalindromo(texto, 0, tamanho - 1)) {
