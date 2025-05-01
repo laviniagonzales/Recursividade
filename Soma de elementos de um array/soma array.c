d6cc05e62c08987cad579aa7fa256a2fe3d8833c
laviniagonzales <laviniagondelima@gmail.com>
2025-04-30 21:44:52 -0300

    Update soma array.c

--- Soma de elementos de um array/soma array.c
+++ Soma de elementos de um array/soma array.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 int somaArray(int arr[], int tamanho) {
-    if (tamanho == 0)  // Caso base: quando o array estiver vazio
+    if (tamanho == 0) 
         return 0;
-    return arr[tamanho - 1] + somaArray(arr, tamanho - 1);  // Soma recursiva
+    return arr[tamanho - 1] + somaArray(arr, tamanho - 1);  
 }
 
 int main() {
-    int arr[] = {1, 2, 3, 4};  // Array de exemplo
-    int tamanho = sizeof(arr) / sizeof(arr[0]);  // Calcula o tamanho do array
+    int arr[] = {1, 2, 3, 4};  
+    int tamanho = sizeof(arr) / sizeof(arr[0]);  
 
     printf("Soma dos elementos do array: %d\n", somaArray(arr, tamanho));
 
